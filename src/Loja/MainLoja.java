11a839ae7fe49c2b668898437451f776cdc41540
Katherine-Martins <katherine.martins@ibm.com>
2022-08-18 10:16:33 -0400

    adicionado comentario do que falta fazer

--- src/Loja/MainLoja.java
+++ src/Loja/MainLoja.java
@@ -48,6 +48,16 @@ public class MainLoja {
             quantidadeProduto = leitor.nextInt();
         }
 
+        /*Lista de Pedidos: Falta printar na tela do usuario */
+        /* Exemplo de saida
+        Order moment: 20/04/2018 11:25:09
+        Order status: PROCESSING
+        Client: Alex Green (15/03/1985) - alex@gmail.com
+        Order items:
+        TV, $1000.00, Quantity: 1, Subtotal: $1000.00
+        Mouse, $40.00, Quantity: 2, Subtotal: $80.00
+        Total price: $1080.00 */
+
         leitor.close();
     }
 }
